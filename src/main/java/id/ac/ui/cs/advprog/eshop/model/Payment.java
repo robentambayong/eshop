182e07d2fb01a8270628d6ee9d0cc14c4a5509b3
robentambayong <tambayongroben@gmail.com>
2026-03-10 16:12:35 +0700

    [RED] Add tests for Payment model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -0,0 +1,22 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import java.util.Map;
+
+@Getter
+public class Payment {
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
+    Order order;
+
+    public Payment(String id, String method, Map<String, String> paymentData, Order order) {
+    }
+
+    public Payment(String id, String method, Map<String, String> paymentData, Order order, String status) {
+    }
+
+    public void setStatus(String status) {
+    }
+}
